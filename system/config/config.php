8333e5950f237bc58c47e19aaf925de84081aac4
a23230101 <a23230101@hotmail.com>
2016-04-03 11:14:30 +0700

    Update config.php

--- system/config/config.php
+++ system/config/config.php
@@ -1,12 +1,12 @@
 <?php
 $config = array();
 $config['type'] = 'config'; 
-$config['1'] = '<a href="">Aaaaaaaaaaaaaa</a>wwwwwwwwww'; 
-$config['2'] = '<a href="http://">aa</a>'; 
-$config['3'] = '<a href="">aaaaaaaaaaaa</a>'; 
+$config['1'] = 'TITLE'; 
+$config['2'] = 'TAGLINE'; 
+$config['3'] = 'DESKRIPSI'; 
 $config['video'] = 'AIzaSyB8nHEdWpbaJ9cTWhofuEFn-4VDFK50DMo, AIzaSyCXyuxq2KTkyT3_EBAm8RYU2h1yJ7Pbc1c, AIzaSyABZWBdTDNSss13Syab9bvRawvHMqFQPFA'; 
 $config['audio'] = 'd9c9bc24cb95527710e48b4448b81968, e8d50db4905ef7cc3f4424099029a599, 6ea8b2cd07dba98905a74dec57407569'; 
-$config['randomsearch'] = 'dj full, dewa 19, a7x live, sak karepe pingine'; 
+$config['randomsearch'] = 'dj full, dewa 19, a7x live'; 
 $config['botch'] = 'Xosig9voYBVvP7CaTALxwBHwxjjCP9uN, 45eA2liYaIIqyM8WnL3Do89lJlsEVI30'; 
 $config['signature'] = 'true'; 
 $config['11'] = 'true'; 
