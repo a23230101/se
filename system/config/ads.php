04e4cd1b40d37782af082b7ff3ee7542d7bc7354
a23230101 <a23230101@hotmail.com>
2016-04-03 10:57:38 +0700

    Added files via upload

---
+++ system/config/ads.php
@@ -0,0 +1,17 @@
+<?php
+$ads = array();
+$ads['type'] = 'ads'; 
+$ads['ads1'] = '<div style="text-align: center;">
+<img src="/system/ads/web.gif">
+</div>'; 
+$ads['ads2'] = '<div style="text-align: center;">
+<img src="/system/ads/wap.gif">
+</div>'; 
+$ads['ads3'] = '<div class="box">
+<a href="#">Mama minta pulsa dong :v</a>
+</div>'; 
+$ads['ads4'] = '<div class="box" style="background:#cfc; color:#060; padding:10px;"><center><a href="#">Gunakan UC Browser Untuk Download Lebih Cepat, Download Sekarang Klik Disini.</a>
+</center></div>'; 
+$ads['la'] = 'http://localhost/andro'; 
+$ads['lo'] = 'http://localhost/else';
+?>
---
+++ system/config/config.php
@@ -0,0 +1,16 @@
+<?php
+$config = array();
+$config['type'] = 'config'; 
+$config['1'] = '<a href="">Aaaaaaaaaaaaaa</a>wwwwwwwwww'; 
+$config['2'] = '<a href="http://">aa</a>'; 
+$config['3'] = '<a href="">aaaaaaaaaaaa</a>'; 
+$config['video'] = 'AIzaSyB8nHEdWpbaJ9cTWhofuEFn-4VDFK50DMo, AIzaSyCXyuxq2KTkyT3_EBAm8RYU2h1yJ7Pbc1c, AIzaSyABZWBdTDNSss13Syab9bvRawvHMqFQPFA'; 
+$config['audio'] = 'd9c9bc24cb95527710e48b4448b81968, e8d50db4905ef7cc3f4424099029a599, 6ea8b2cd07dba98905a74dec57407569'; 
+$config['randomsearch'] = 'true'; 
+$config['botch'] = 'Xosig9voYBVvP7CaTALxwBHwxjjCP9uN, 45eA2liYaIIqyM8WnL3Do89lJlsEVI30'; 
+$config['signature'] = 'true'; 
+$config['11'] = 'true'; 
+$config['headtag'] = '<meta name=author content="Mark Otto, Jacob Thornton, and Bootstrap contributors">'; 
+$config['random'] = '5'; 
+$config['total'] = '5'; 
+?>
